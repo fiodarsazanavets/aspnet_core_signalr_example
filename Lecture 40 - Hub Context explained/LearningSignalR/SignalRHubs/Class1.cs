a719072b8a216654dfb4d6336878e5d85d14a3d4
mobiletechtracker <info@mobiletechtracker.co.uk>
2020-07-02 18:41:41 +0100

    Add the code samples.

---
+++ Lecture 40 - Hub Context explained/LearningSignalR/SignalRHubs/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SignalRHubs
+{
+    public class Class1
+    {
+    }
+}
---
+++ Lecture 5 - Introduction to SignalR Hub/LearningSignalR/LearningHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace LearningSignalR
+{
+    public class LearningHub  : Hub
+    {
+        public Task BroadcastMessage(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
